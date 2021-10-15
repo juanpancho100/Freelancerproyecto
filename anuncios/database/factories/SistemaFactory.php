f45412abee49afd7119f63847fbb3bb45d6bc846
juanpancho100 <juanpanchoguti@gmail.com>
2021-10-15 02:04:52 -0500

    version1

---
+++ anuncios/database/factories/SistemaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Sistema;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SistemaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Sistema::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
