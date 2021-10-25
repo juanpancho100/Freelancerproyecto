e49d1eb9fafcb6cccdda1878db82e5adb3d4097a
juanpancho100 <juanpanchoguti@gmail.com>
2021-10-25 01:12:20 -0500

    version2

---
+++ anuncios/resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="https://stickershop.line-scdn.net/stickershop/v1/product/1043153/LINEStorePC/main.png;compress=true" width="50px" height="50px">
---
+++ anuncios/resources/views/dashboard.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Anuncios Recientes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="container">
+                <div class="p-10 bg-white">
+                        Anuncios 1
+                        <div class="col">
+                        foto
+                        </div>
+                        <div class="col">
+                        nombre
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Anuncios 2                   
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Anuncios 3                   
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
